d7e3f6a0efd7b8e7b9cef45e0b06c8b8d2364557
MixerPixel220 <maxuswill@yandex.ru>
2026-03-23 18:31:33 +0700

    add creating and stats

---
+++ js/History.js
@@ -0,0 +1,30 @@
+class Histories {
+    histories = [
+        {
+            id: "ifkdskmfks",
+            title: "Кодинг по-кошачьи",
+            desc: "Аня уходит в магазин за продуктами, забывая закрыть проект на компьютере. Любозательный МяуМяф решается дописать код, пока хозяйка не вернулась...",
+            img: "assets/his1.png",
+        },
+        {
+            id: "fkslifksdlls",
+            title: "Хочешь жить - умей мяукать",
+            desc: "В то время, как Аню грозятся уволить за сломанный проект, Мяумяф отправляется в компанию, чтобы спасти её карьеру...",
+            img: "assets/his2.png",
+        },
+    ];
+
+    addHistory = (history) => {
+        return this.histories.push(history);
+    };
+
+    deleteHistory = (id) => {
+        this.histories = this.histories.filter((el) => el.id !== id);
+    };
+
+    shapeHistoryObj = (title, desc, img) => {
+        return { id: crypto.randomUUID(), title, desc, img };
+    };
+}
+
+export const myHistories = new Histories();
